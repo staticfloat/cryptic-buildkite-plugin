5aafab5ed221f1f5aaa1070cb927778a2bc216a3
Elliot Saba <staticfloat@gmail.com>
2021-05-28 20:37:58 +0000

    Begin implementing locked pipeline launching

--- lib/common.sh
+++ lib/common.sh
@@ -2,6 +2,8 @@
 
 set -eou pipefail
 shopt -s extglob
+shopt -s globstar
+
 
 # Helper function to kill execution when something goes wrong
 function die() {
@@ -15,6 +17,15 @@ function die() {
 # We require `openssl` for basically everything
 if [[ -z "$(which openssl 2>/dev/null)" ]]; then
     die "'openssl' tool required!"
+fi
+
+# Figure out which shasum program to use
+if [[ -n $(which sha256sum 2>/dev/null) ]]; then
+    SHASUM="sha256sum"
+elif [[ -n $(which shasum 2>/dev/null) ]]; then
+    SHASUM="shasum -a 256"
+else
+    die "No sha256sum/shasum available!"
 fi
 
 
@@ -131,10 +142,75 @@ function gen_encrypted_aes_key() {
     openssl rand 128 | encrypt_rsa "${1}"
 }
 
+##############################################################################
+##############                   treehashing                    ##############
+##############################################################################
+
+
+function collect_glob_pattern() {
+    # First argument is either a glob pattern, or a directory.  If it is a directory,
+    # we add `/**/*` to it in order to select everything underneath it.
+    local target="${1}"
+    local prefix="${1}"
+    if [[ -d "${target}" ]]; then
+        target="${target}/**/*"
+    fi
+
+    # Iterate over the glob pattern
+    for f in ${target}; do
+        # Ignore directories, only list files
+        if [[ -f ${f} ]]; then
+            printf "%s\0" "${f}"
+        fi
+    done
+}
+
+function calc_shasum() {
+    ${SHASUM} "$@" | awk '{ print $1 }'
+}
+
+# poor man's treehash of a set of files; use with `collect_glob_pattern`
+function calc_treehash() {
+    # Fill `FILES` with all the files we're calculating the treehash over
+    readarray -d '' FILES
+
+    # If we have no files, exit early!
+    if [[ "${#FILES[@]}" == 0 ]]; then
+        calc_shasum < /dev/null | awk '{ print $1 }'
+        return
+    fi
+
+    # Next, we fold things up into directories
+    declare -A DIR_HASHES
+    for f in $(sort <<< "${FILES[@]}"); do
+        hash=$(calc_shasum "${f}" | awk '{ print $1 }')
+        dir=$(dirname "${f}")
+        DIR_HASHES["${dir}"]+=" $(basename ${f}) ${hash}"
+    done
+
+    # Collapse directories into their parents until none survive
+    while [[ ${#DIR_HASHES[@]} -gt 1 ]]; do
+        DIRS=$(tr ' ' '\n' <<< "${!DIR_HASHES[@]}")
+        for f in $(sort <<< "${DIRS}"); do
+            # If this directory appears only once, move it up to its parent
+            if [[ "$(egrep "^${f}" <<< "${DIRS}")" == "${f}" ]]; then
+                dir=$(dirname "${f}")
+                hash=$( calc_shasum <<< "${DIR_HASHES["${f}"]}" | awk '{ print $1 }')
+                DIR_HASHES["${dir}"]+=" $(basename ${f}) ${hash}"
+                unset DIR_HASHES["${f}"]
+            fi
+        done
+    done
+
+    calc_shasum <<< ${DIR_HASHES[@]}
+}
+
 
 ##############################################################################
 ##############                random utilities                  ##############
 ##############################################################################
+
+# Print file size, in bytes
 function filesize() {
     du -b "${1}" | cut -f1
 }
